6af88b433d5a5a8d0ecbe3ec7001a14c25c8b9eb
qydq <qyddai@163.com>
2019-11-27 22:29:26 +0800

    adad

---
+++ library/src/main/java/com/test1/PublicMainInfoUtils.java
@@ -0,0 +1,14 @@
+package com.test1;
+
+/**
+ * @author sunst
+ * @date 2019/11/26 10:19.
+ * email：qyddai@gmail.com
+ * description：
+ */
+public class PublicMainInfoUtils {
+
+    public static String getAuthor() {
+        return "sunshuntao";
+    }
+}
